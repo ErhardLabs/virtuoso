cf73faa82b63cb9c8b684fef9bdf7f3cc1736a0a
Sumner <serhard2556@gmail.com>
2018-09-08 13:31:59 -0600

    Cleaning up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,3 @@
 $ = jQuery;
 import './menu/app.js';
 import './sexy-animations/app.js';
-
-$(document).ready(function() {
-  // $( 'body' ).removeClass( 'virtuoso-no-js' );
-  // $( 'body' ).addClass( 'virtuoso-js' );
-  // $( 'body' ).addClass( 'js' );
-});
