9ad2c7090fb2a294c206af7690a2df244d045f7c
Sumner <serhard2556@gmail.com>
2018-09-16 20:24:17 -0600

    Four Column footer is complete. Need to add footer-design configuration parameters to the theme to make the footer configurable.

---
+++ lib/views/footer-copyright.php
@@ -0,0 +1,4 @@
+<p>&copy; Copyright <?php echo date( "Y" ) ?>
+    <a href="<?php bloginfo( 'url' ); ?>"><?php bloginfo(); ?></a>
+    &middot; All Rights Reserved &middot; Design & Development by <a href="https://sumnererhard.com/">Erhard Labs</a>
+</p>
