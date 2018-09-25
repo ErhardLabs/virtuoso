9c41a46a2e6669f2774eddd8aca25450749b6f2e
Grayson Erhard <web@graysonerhard.com>
2018-09-24 19:53:32 -0600

    autoplay working, needs to be polished

--- assets/js/BackgroundVideo.js
+++ assets/js/BackgroundVideo.js
@@ -1,95 +1,123 @@
-$=jQuery;
+class BackgroundVideo {
 
-$(document).ready(function() {
+  constructor() {
 
-  let homeVideo = $('#landing_yt_player').data('id');
-  let playlistID = $('#landing_yt_player').data('playlist-id');
-  let homeVideoStartTime = $('#landing_yt_player').data('start-time');
-  let thumb = '<img src="https://img.youtube.com/vi/'+homeVideo+'/maxresdefault.jpg">';
-  let thumbSrc = 'https://img.youtube.com/vi/'+homeVideo+'/maxresdefault.jpg';
+    this.homeVideo = $('#landing_yt_player').data('id');
+    this.playlistID = $('#landing_yt_player').data('playlist-id');
+    this.homeVideoStartTime = $('#landing_yt_player').data('start-time');
+    // this.thumb = '<img src="https://img.youtube.com/vi/'+homeVideo+'/maxresdefault.jpg">';
+    // this.thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">';
+    this.thumb = '<img src="https://img.youtube.com/vi/'+this.homeVideo+'/0.jpg">';
+    this.thumbSrc = 'https://img.youtube.com/vi/'+this.homeVideo+'/maxresdefault.jpg';
+    this.play = '<div class="play"></div>';
 
-  if (typeof homeVideo !== 'undefined') {
+    this.addVideoHTML();
 
-    if ($(window).width() < 768) {
+    // Loads the IFrame Player API code asynchronously.
+    BackgroundVideo.loadIframePlayerAPI();
 
-      $('body').prepend(
-          '<div class="video-background" style="background-image: url('+thumbSrc+')">' +
-          '<div class="video-foreground" >' +
-          '</div>' +
-          '</div>'
-      );
+    this.processIframe();
 
-    } else {
+  }
 
-      let fullPath = location.pathname + location.search + location.hash;
+  addVideoHTML() {
 
-      // INSERT VIDEO PLAYER
-      $('body').prepend(
-          '<div class="video-background">' +
-          '<div class="video-foreground">' +
-          '<iframe class="home_video" id="yt_home_embed" width="1280" height="720" src="https://www.youtube.com/embed/' + homeVideo + '?rel=0&controls=0&showinfo=0&autoplay=1&loop=1&enablejsapi=1&playlist='+playlistID+'&start='+homeVideoStartTime+'" frameborder="0" allowfullscreen volume="0"></iframe>' +
-          '</div>' +
-          '</div>'
-      );
+    if (typeof this.homeVideo !== 'undefined') {
+
+      if ($(window).width() < 768) {
+
+        $('body').prepend(
+            '<div class="video-background" style="background-image: url('+this.thumbSrc+')">' +
+            '<div class="video-foreground" >' +
+            '</div>' +
+            '</div>'
+        );
+
+      } else {
+
+        let src = 'https://www.youtube.com/embed/' + this.homeVideo + '?rel=0&controls=0&showinfo=0&autoplay=1&disablekb=1&loop=1&enablejsapi=1';
+
+        if (this.playlistID !== '') {
+          src += '&playlist=' + this.playlistID;
+        }
+
+        if (this.homeVideoStartTime !== '') {
+          src += '&start='+this.homeVideoStartTime;
+        }
+
+
+        // INSERT VIDEO PLAYER
+        $('body').prepend(
+            '<div class="video-background">' +
+            '<div class="video-foreground">' +
+            '<iframe class="home_video" id="yt_home_embed" width="1280" height="720" src="'+src+'" frameborder="0" allowfullscreen></iframe>' +
+            '</div>' +
+            '</div>'
+        );
+
+      }
 
     }
 
-    // Loads the IFrame Player API code asynchronously.
-    let tag = document.createElement('script');
+  }
 
+  // Loads the IFrame Player API code asynchronously.
+  static loadIframePlayerAPI() {
+    let tag = document.createElement('script');
     tag.src = "https://www.youtube.com/iframe_api";
     let firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    console.log(tag);
+  }
 
-    let player;
 
-    // LOAD iFrame after content loads and a button is clicked
-    let div, n,
-        v = document.getElementsByClassName("youtube-player");
-    for (n = 0; n < v.length; n++) {
-      div = document.createElement("div");
-      div.setAttribute("data-id", v[n].dataset.id);
-      div.innerHTML = labnolThumb(v[n].dataset.id);
-      div.onclick = labnolIframe;
-      v[n].appendChild(div);
-    }
+  processIframe() {
+
+    let div;
+    let n;
+    this.player = {};
+    let v = document.getElementById("yt_home_embed");
+    let id = v.dataset.id;
+
+
+    div = document.createElement("div");
+    div.setAttribute("data-id", id);
+    div.innerHTML = this.thumb.replace("ID", id) + this.play;
+
+    let self = this;
+
+    div.onload = function() {
+      let iframe = document.createElement("iframe");
+      let embed = "https://www.youtube.com/embed/videoseries?list="+self.playlistID;
+      iframe.setAttribute("src", embed);
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute("allowfullscreen", "1");
+      console.log(this);
+      this.parentNode.replaceChild(iframe, this);
+  };
+
+    v.appendChild(div);
 
 
   }
 
 
-});
+}
 
-function onYouTubeIframeAPIReady() {
+
+new BackgroundVideo();
+
+window.onYouTubeIframeAPIReady = function() {
+  document.dispatchEvent(new CustomEvent('onYouTubeIframeAPIReady', {}))
+
+  console.log('YT API loaded!');
   let player = new YT.Player('yt_home_embed', {
     events: {
-      'onReady': onPlayerReady
+      'onReady': function() {
+        // Mute!
+        player.mute();
+        player.playVideo();
+      }
     }
   });
-}
-
-function onPlayerReady() {
-  // Mute!
-  player.mute();
-  player.playVideo();
-}
-
-
-function labnolThumb(id) {
-  let homeVideo = $('#landing_yt_player').data('id');
-  // let thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">';
-  let thumb = '<img src="https://img.youtube.com/vi/'+homeVideo+'/0.jpg">';
-  let play = '<div class="play"></div>';
-  return thumb.replace("ID", id) + play;
-}
-
-function labnolIframe() {
-
-  let playlistID = $('#landing_yt_player').data('playlist-id');
-  let iframe = document.createElement("iframe");
-  let embed = "https://www.youtube.com/embed/videoseries?list="+playlistID;
-  iframe.setAttribute("src", embed);
-  iframe.setAttribute("frameborder", "0");
-  iframe.setAttribute("allowfullscreen", "1");
-  this.parentNode.replaceChild(iframe, this);
-}
+};
