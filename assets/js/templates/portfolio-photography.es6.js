98b11ffaa59e135205d000a9a9e480a745536af0
Grayson Erhard <web@graysonerhard.com>
2018-10-23 21:53:57 -0600

    created todo

--- assets/js/templates/portfolio-photography.es6.js
+++ assets/js/templates/portfolio-photography.es6.js
@@ -2,10 +2,11 @@
 //   console.log($(this));
 // });
 
+// TODO: MAKE THIS JQUERY SELECTOR MORE TEMPLATE-SPECIFIC
+
 $('.wp-block-gallery').slick({
   arrows: true,
   fade: true,
   prevArrow: '',
   nextArrow: '',
-
 });
