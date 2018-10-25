12c1f4d15686b32262eb913e7b6c1bc54c5b1edf
Sumner <serhard2556@gmail.com>
2018-10-25 13:25:24 -0600

    Basic slider styles

--- assets/js/templates/category-photography.es6.js
+++ assets/js/templates/category-photography.es6.js
@@ -1,6 +1,7 @@
 // TODO: MAKE THIS JQUERY SELECTOR MORE TEMPLATE-SPECIFIC
 
 $('.photography_slider_wrap').slick({
+  // adaptiveHeight: true,
   arrows: true,
   fade: true,
   prevArrow: '<i class="prevArrow ti-angle-left"></i>',
--- assets/js/templates/portfolio-photography.es6.js
+++ assets/js/templates/portfolio-photography.es6.js
@@ -3,6 +3,9 @@
 $('.wp-block-gallery').slick({
   arrows: true,
   fade: true,
+  slidesToShow: 2,
+  slidesToScroll: 1,
   prevArrow: '<i class="prevArrow ti-angle-left"></i>',
   nextArrow: '<i class="nextArrow ti-angle-right"></i>',
+  // mobileFirst: true
 });
