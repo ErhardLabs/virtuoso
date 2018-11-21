d385f4841ab721c78b35d5ebf56e9b7a12cd14ec
Sumner <serhard2556@gmail.com>
2018-11-20 17:34:57 -0700

    Removed sexy animations from the theme and updated the body fade in

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,10 @@
-$ = jQuery;
+$=jQuery;
+
+jQuery(document).ready(function () {
+  jQuery('body').addClass('body-loaded');
+});
+
 import './menu/app.js';
-import './sexy-animations/app.js';
 import './BackgroundVideo.js';
 import './slide-out-sidebar/Slide_Out_Sidebar.es6.js';
 import './templates/app.js';
