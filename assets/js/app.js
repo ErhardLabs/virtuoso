52765c85a5bea27c64ebb6394db80bde0d73b980
Sumner <serhard2556@gmail.com>
2019-01-14 15:45:35 -0700

    Created a parallax mountain 404 page. Should keep visitors on for a couple more seconds

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,4 @@ import './BackgroundVideo.js';
 import './slide-out-sidebar/Slide_Out_Sidebar.es6.js';
 import './templates/app.js';
 import './slick-slider-arrow-keys.js';
+import './MouseOverParallax.js';
