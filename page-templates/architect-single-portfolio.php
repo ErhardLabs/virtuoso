3db46cccffc9e14531e5dd2f5084cfa2069a71b4
Sumner <serhard2556@gmail.com>
2019-01-25 09:20:37 -0700

    Updated classes and moved some content around for the architect single portfolio page

--- page-templates/architect-single-portfolio.php
+++ page-templates/architect-single-portfolio.php
@@ -12,36 +12,63 @@ remove_action( 'genesis_entry_header', 'genesis_do_post_title' ); // remove titl
 remove_action( 'genesis_entry_header', 'genesis_post_info', 12 ); // remove date, post meta
 remove_action('genesis_entry_footer', 'genesis_post_meta', 10); // remove post meta footer
 
+add_action('genesis_after_header', 'virtuoso_architect_single_portfolio_title');
+
+function virtuoso_architect_single_portfolio_title() {
+  ?>
+
+    <div class="title_wrap mobile">
+        <h1><?php the_title(); ?></h1>
+    </div>
+
+  <?php
+}
+
 add_action('genesis_after_entry', 'virtuoso_architect_single_portfolio');
 
 function virtuoso_architect_single_portfolio() {
 
   ?>
 
-    <div class="portfolio_info_wrap wrap">
-      <div class="title_wrap">
+    <div class="portfolio_info_wrap wrap desktop">
+      <div class="title_wrap desktop">
         <h1><?php the_title(); ?></h1>
       </div>
       <div class="info_wrap_left">
-        <div class="location_wrap">
-          <h4>Location:</h4>
-          <span><?php echo get_field('location'); ?></span>
-        </div>
-        <div class="category_wrap">
-          <h4>Category:</h4>
-          <span><?php echo get_field('category'); ?></span>
-        </div>
-        <div class="completion_wrap">
+
+        <?php if ( get_field('location') ): ?>
+            <div class="location_wrap info_container">
+              <h4>Location:</h4>
+              <span><?php echo get_field('location'); ?></span>
+            </div>
+        <?php endif; ?>
+
+        <?php if ( get_field('category') ): ?>
+            <div class="category_wrap info_container">
+              <h4>Category:</h4>
+              <span><?php echo get_field('category'); ?></span>
+            </div>
+        <?php endif; ?>
+
+	    <?php if ( get_field('completion') ): ?>
+        <div class="completion_wrap info_container">
           <h4>Completion:</h4>
           <span><?php echo get_field('completion'); ?></span>
         </div>
-      </div>
+		    <?php endif; ?>
+
+      </div> <!-- info_wrap_left -->
+
+
       <div class="info_wrap_right">
-        <div class="description_wrap">
-          <img src="" />
-          <?php echo get_field('description'); ?>
-        </div>
-      </div>
+	     <?php if ( get_field('description') ): ?>
+            <div class="description_wrap">
+              <div class="yellow-accent"></div>
+             <?php echo get_field('description'); ?>
+            </div>
+		 <?php endif; ?>
+      </div> <!-- info_wrap_right -->
+
     </div>
 
   <?php
