4f2cd92faf4712f3a6548df28597146bfc636ec0
Sumner <serhard2556@gmail.com>
2019-01-28 03:36:38 -0700

    Theme now supports dynamic anchor scrolling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,3 @@
-$( document ).ready( function() {
-  $( 'body' ).addClass( 'body-loaded' );
-});
-
 import './body-loaded.js';
 import './menu/app.js';
 import './BackgroundVideo.js';
@@ -9,3 +5,4 @@ import './slide-out-sidebar/Slide_Out_Sidebar.es6.js';
 import './templates/app.js';
 import './slick-slider-arrow-keys.js';
 import './MouseOverParallax.js';
+import './anchor-scroll.js';
