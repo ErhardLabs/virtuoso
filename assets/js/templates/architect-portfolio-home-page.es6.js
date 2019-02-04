87bb165bf3067518aeb2d3258bdaa81572258d66
Sumner <serhard2556@gmail.com>
2019-02-04 05:17:05 -0700

    Architect - cleaning out old home page js code

--- assets/js/templates/architect-portfolio-home-page.es6.js
+++ assets/js/templates/architect-portfolio-home-page.es6.js
@@ -4,19 +4,6 @@ $( '.home .architecture_slider_wrap' ).slick({
   dots: true,
   autoplay: true,
   autoplaySpeed: 4000
-
-  // appendArrows: '.home .slider-nav',
-  // appendDots: '.home .slider-nav',
-  // prevArrow: '<i class="prevArrow ti-angle-left"></i>',
-  // nextArrow: '<i class="nextArrow ti-angle-right"></i>'
-});
-
-$( '.js-slick' ).on( 'beforeChange', function( event, slick, currentSlide, nextSlide ) {
-  $( slick.$slides ).removeClass( 'is-animating' );
-});
-
-$( '.js-slick' ).on( 'afterChange', function( event, slick, currentSlide, nextSlide ) {
-    $( slick.$slides.get( currentSlide ) ).addClass( 'is-animating' );
 });
 
 // Move Living to the first position.
