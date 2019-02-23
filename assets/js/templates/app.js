5dc72150caf36f5bccbc47d3454cc0dcd53743a6
Sumner <serhard2556@gmail.com>
2019-02-22 21:47:42 -0700

    Single artist page finished. Added new ACF fields and completed the slider functionality

--- assets/js/templates/app.js
+++ assets/js/templates/app.js
@@ -4,3 +4,4 @@ import './portfolio-photography.es6.js';
 import './portfolio-videography.es6.js';
 import './single-portfolio.es6.js';
 import './architect-portfolio-home-page.es6.js';
+import './publisher-single-artist.es6.js';
---
+++ page-templates/publisher-single-artist.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Template Name: Publisher - Single Artist
+ * Template Post Type: artists
+ */
+
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' ); // remove title
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 ); // remove date, post meta
+remove_action('genesis_entry_footer', 'genesis_post_meta', 10); // remove post meta footer
+
+add_action('genesis_after_header', 'virtuoso_architect_single_portfolio_title');
+
+function virtuoso_architect_single_portfolio_title() {
+	?>
+	<div class="spacing"></div>
+	<?php
+}
+
+add_action('genesis_before_entry_content', 'virtuoso_publisher_single_artist');
+
+function virtuoso_publisher_single_artist() {
+	$artist_website = get_field('artist_website');
+	$artist_bio = get_field("artist_bio");
+	?>
+    <section class="single-artist-content">
+        <section class="artist-profile">
+            <div class="wrap">
+                <div class="title-wrap">
+                    <h1><?php the_title(); ?></h1>
+                </div>
+
+                <?php if ( have_rows( 'artist_social_media_links') ): ?>
+                <div class="social-media-links_wrap">
+                    <?php while( have_rows( 'artist_social_media_links' ) ): the_row(); ?>
+                        <a class="social-button" href="<?php the_sub_field('link') ?>"><i class="<?php the_sub_field('icon') ?>"></i></a>
+                    <?php endwhile; ?>
+                </div>
+                <?php endif; ?>
+
+                <?php if ( $artist_website ): ?>
+                    <div class="artist-website_wrap">
+                        <a href="https://<?php echo $artist_website; ?>"><?php echo $artist_website?></a>
+                    </div>
+                <?php endif; ?>
+
+	            <?php if ( have_rows( 'artist_music_embed_links') ): ?>
+                <div class="artist-music_wrap">
+                    <div class="heading_wrap">
+                        <h2>Music</h2>
+                        <h4>Top Songs</h4>
+                    </div>
+	                <?php while( have_rows( 'artist_music_embed_links' ) ): the_row(); ?>
+                        <div class="bwpp_iframe_wrap song">
+                              <iframe style="border: 0; width: 100%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/track=<?php the_sub_field('song_id'); ?>/size=small/bgcol=0B0C10/linkcol=ffffff/transparent=true/" seamless></iframe>
+                        </div>
+	                <?php endwhile; ?>
+                </div>
+                <?php endif; ?>
+
+            </div>
+        </section>
+
+        <?php if ( have_rows( 'artist_videos' ) ): ?>
+        <section class="artist-video-playlist-slider">
+            <div class="wrap">
+                <div class="heading_wrap">
+                    <h2>Videos</h2>
+                </div>
+                <div class="videos_wrap">
+                    <?php while( have_rows( 'artist_videos' ) ): the_row(); ?>
+                        <div class="video">
+                            <div class="embed-container">
+                              <?php the_sub_field('artist_video') ?>
+                            </div>
+                        </div>
+                    <?php endwhile; ?>
+                </div>
+            </div>
+        </section>
+        <?php endif; ?>
+
+        <?php if ($artist_bio): ?>
+        <section class="artist-bio">
+            <div class="wrap">
+                <div class="heading_wrap">
+                    <h2>Bio</h2>
+                </div>
+                <div class="bio_wrap">
+                    <p><?php echo $artist_bio ?></p>
+                </div>
+            </div>
+        </section>
+        <?php endif; ?>
+    </section> <!-- End of single-artist-content -->
+
+	<?php
+
+}
+
+genesis();
