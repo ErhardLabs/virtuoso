999535509f6e99c43ac40407aca2fb72e4b5e3d2
Grayson Erhard <web@graysonerhard.com>
2019-04-24 15:15:05 -0600

    fixed error in sidebar and added tunefuze front page

---
+++ page-templates/tunefuze-front-page.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Template Name: Tunefuze - Front Page
+ * Template Post Type: page
+ */
+
+// REMOVE ENTIRE CONTAINER FOR ENTRY HEADER
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+//add_action( 'the_content', function() {} ); // clear content so nothing displays
+
+add_action( 'genesis_before_entry_content', 'virtuoso_publisher_front_page_content' );
+function virtuoso_publisher_front_page_content() {
+
+  if (is_front_page()) {
+
+    add_filter( 'body_class', function( $classes ) {
+      return array_merge( $classes, array( 'tunefuze', 'all_fuzes' ) );
+    } );
+
+//    $profileOwner = ((int) buddypress()->displayed_user->id === (int) buddypress()->loggedin_user->userdata->ID);
+
+    if (is_user_logged_in()) {
+      ?>
+      <h2>Recommended</h2>
+      <div id="recommended_fuzes"></div>
+      <h2>Trending</h2>
+      <div id="trending_fuzes"></div>
+      <h2>Curated</h2>
+      <div id="curated_fuzes"></div>
+      <h2>Following</h2>
+      <div id="following_fuzes"></div>
+      <h2>All-Time Most Viewed</h2>
+      <div id="all_time_most_viewed_fuzes"></div>
+      <h2>All-Time Most Liked</h2>
+      <div id="all_time_most_liked_fuzes"></div>
+      <?php
+    } else {
+      ?>
+      <h2>Trending</h2>
+      <div id="trending_fuzes"></div>
+      <h2>Curated</h2>
+      <div id="curated_fuzes"></div>
+      <h2>All-Time Most Viewed</h2>
+      <div id="all_time_most_viewed_fuzes"></div>
+      <h2>All-Time Most Liked</h2>
+      <div id="all_time_most_liked_fuzes"></div>
+      <?php
+    }
+
+  }
+
+}
+
+genesis();
