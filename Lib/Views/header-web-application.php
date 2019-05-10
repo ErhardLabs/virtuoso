1d2484c31997b3924a592c0fd5402b495842f289
Sumner <develop@sumnererhard.com>
2019-05-10 11:34:17 -0600

    Cleaning up code

--- Lib/Views/header-web-application.php
+++ Lib/Views/header-web-application.php
@@ -1,9 +1,3 @@
-<!--<h1 class="logo-container">-->
-<!--	<a href="/">-->
-<!--        <img src=""/>-->
-<!--    </a>-->
-<!--</h1>-->
-
 <div id="site-navigation" class="navigation-container">
 	<?php
 		genesis_do_nav();
