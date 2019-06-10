ea8afc0f9f9804177c94ec252ff50522064bcd77
Sumner <develop@sumnererhard.com>
2019-06-10 16:02:21 -0600

    Added some classes into the tunefuze home page template

--- page-templates/tunefuze-front-page.php
+++ page-templates/tunefuze-front-page.php
@@ -9,44 +9,44 @@ remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
 remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
 remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
 
-//add_action( 'the_content', function() {} ); // clear content so nothing displays
+if ( is_front_page() ) {
+	add_filter( 'body_class', function( $classes ) {
+		return array_merge( $classes, array( 'tunefuze', 'all_fuzes', 'home' ) );
+	} );
+}
 
 add_action( 'genesis_before_entry_content', 'virtuoso_publisher_front_page_content' );
 function virtuoso_publisher_front_page_content() {
 
   if (is_front_page()) {
 
-    add_filter( 'body_class', function( $classes ) {
-      return array_merge( $classes, array( 'tunefuze', 'all_fuzes' ) );
-    } );
-
 //    $profileOwner = ((int) buddypress()->displayed_user->id === (int) buddypress()->loggedin_user->userdata->ID);
 
     if (is_user_logged_in()) {
       ?>
       <h2>Recommended</h2>
-      <div id="recommended_fuzes"></div>
+      <div class="fuze-group" id="recommended_fuzes"></div>
       <h2>Trending</h2>
-      <div id="trending_fuzes"></div>
+      <div class="fuze-group" id="trending_fuzes"></div>
       <h2>Curated</h2>
-      <div id="curated_fuzes"></div>
+      <div class="fuze-group" id="curated_fuzes"></div>
       <h2>Following</h2>
-      <div id="following_fuzes"></div>
+      <div class="fuze-group" id="following_fuzes"></div>
       <h2>All-Time Most Viewed</h2>
-      <div id="all_time_most_viewed_fuzes"></div>
+      <div class="fuze-group" id="all_time_most_viewed_fuzes"></div>
       <h2>All-Time Most Liked</h2>
-      <div id="all_time_most_liked_fuzes"></div>
+      <div class="fuze-group" id="all_time_most_liked_fuzes"></div>
       <?php
     } else {
       ?>
       <h2>Trending</h2>
-      <div id="trending_fuzes"></div>
+      <div class="fuze-group" id="trending_fuzes"></div>
       <h2>Curated</h2>
-      <div id="curated_fuzes"></div>
+      <div class="fuze-group" id="curated_fuzes"></div>
       <h2>All-Time Most Viewed</h2>
-      <div id="all_time_most_viewed_fuzes"></div>
+      <div class="fuze-group" id="all_time_most_viewed_fuzes"></div>
       <h2>All-Time Most Liked</h2>
-      <div id="all_time_most_liked_fuzes"></div>
+      <div class="fuze-group" id="all_time_most_liked_fuzes"></div>
       <?php
     }
 
