8eb9c0bc82ad75dab42faa65b8994cbb9a97f23f
Grayson Erhard <web@graysonerhard.com>
2020-03-04 11:44:28 -0700

    Fix: Mobile usability issues in footer.

--- Lib/Views/footer-copyright.php
+++ Lib/Views/footer-copyright.php
@@ -1,8 +1,6 @@
-<!--<div class="footer-copyright stacked">-->
-    <p>&copy; <?php echo date( "Y" ) ?> <?php bloginfo(); ?></a></p>
-    <div class="footer-agency-copyright">
-        <p>Design & development by <a href="https://erhardlabs.com/">Erhard Labs</a></p>
-    </div>
-    <p>All Rights Reserved</p>
-    <p><a href="/wp-admin" class="footer-login">Login</a></p>
-<!--</div>-->
+<p>&copy; <?php echo date( "Y" ) ?> <?php bloginfo(); ?></a></p>
+<div class="footer-agency-copyright">
+    <p>Design & development by <a href="https://erhardlabs.com/">Erhard Labs</a></p>
+</div>
+<p>All Rights Reserved</p>
+<p><a href="/wp-admin" class="footer-login">Login</a></p>
