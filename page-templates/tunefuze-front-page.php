83f5173579807e49ee22b758a007839858ded6d3
Sumner Erhard <develop@sumnererhard.com>
2020-04-03 15:10:14 -0600

    adjusting tunefuze front page template

--- page-templates/tunefuze-front-page.php
+++ page-templates/tunefuze-front-page.php
@@ -25,29 +25,49 @@ function virtuoso_publisher_front_page_content() {
 
 		if ( is_user_logged_in() ) {
 			?>
-	  <h2>Recommended</h2>
-	  <div class="fuze-group" id="recommended_fuzes"><div class="fuze-group-wrap"></div></div>
-	  <h2>Trending</h2>
-	  <div class="fuze-group" id="trending_fuzes"><div class="fuze-group-wrap"></div></div>
-	  <h2>Curated</h2>
-	  <div class="fuze-group" id="curated_fuzes"><div class="fuze-group-wrap"></div></div>
-	  <h2>Following</h2>
-	  <div class="fuze-group" id="following_fuzes"><div class="fuze-group-wrap"></div></div>
-	  <h2>All-Time Most Viewed</h2>
-	  <div class="fuze-group" id="all_time_most_viewed_fuzes"><div class="fuze-group-wrap"></div></div>
-	  <h2>All-Time Most Liked</h2>
-	  <div class="fuze-group" id="all_time_most_liked_fuzes"><div class="fuze-group-wrap"></div></div>
+            <div class="recommended fuze-group-container">
+                  <h2>Recommended</h2>
+                  <div class="fuze-group" id="recommended_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="trending fuze-group-container">
+                  <h2>Trending</h2>
+                  <div class="fuze-group" id="trending_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="curated fuze-group-container">
+                  <h2>Curated</h2>
+                  <div class="fuze-group" id="curated_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="following fuze-group-container">
+	            <h2>Following</h2>
+	            <div class="fuze-group" id="following_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="all_time_most_viewed fuze-group-container">
+                  <h2>All-Time Most Viewed</h2>
+                  <div class="fuze-group" id="all_time_most_viewed_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="all_time_most_liked fuze-group-container">
+                  <h2>All-Time Most Liked</h2>
+                  <div class="fuze-group" id="all_time_most_liked_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
 			<?php
 		} else {
 			?>
-		<h2>Trending</h2>
-		<div class="fuze-group" id="trending_fuzes"><div class="fuze-group-wrap"></div></div>
-		<h2>Curated</h2>
-		<div class="fuze-group" id="curated_fuzes"><div class="fuze-group-wrap"></div></div>
-		<h2>All-Time Most Viewed</h2>
-		<div class="fuze-group" id="all_time_most_viewed_fuzes"><div class="fuze-group-wrap"></div></div>
-		<h2>All-Time Most Liked</h2>
-		<div class="fuze-group" id="all_time_most_liked_fuzes"><div class="fuze-group-wrap"></div></div>
+            <div class="trending fuze-group-container">
+                <h2>Trending</h2>
+                <div class="fuze-group" id="trending_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="curated fuze-group-container">
+                <h2>Curated</h2>
+                <div class="fuze-group" id="curated_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="all_time_most_viewed fuze-group-container">
+                <h2>All-Time Most Viewed</h2>
+                <div class="fuze-group" id="all_time_most_viewed_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
+            <div class="all_time_most_liked fuze-group-container">
+                <h2>All-Time Most Liked</h2>
+                <div class="fuze-group" id="all_time_most_liked_fuzes"><div class="fuze-group-wrap"></div></div>
+            </div>
 			<?php
 		}
 	}
