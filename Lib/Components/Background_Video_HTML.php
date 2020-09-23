a811abda47e95ebedda4aedcf23c179523fae655
Grayson Erhard <web@graysonerhard.com>
2020-09-23 17:32:55 -0600

    Fix: Fixed if ACF and BuddyPress isn't installed, the site would break.

--- Lib/Components/Background_Video_HTML.php
+++ Lib/Components/Background_Video_HTML.php
@@ -15,84 +15,92 @@ use Virtuoso\Lib\Admin\Customizer\CustomizerHelpers;
 use Virtuoso\Lib\Components\Background_Image;
 use Virtuoso\Lib\Helper;
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Class Background_Video_HTML.
  */
-class Background_Video_HTML {
+class Background_Video_HTML
+{
 
-	public $video_id;
-	public $playlist_id;
-	public $start_time;
-	public $below_header;
-	public $blur_vid_bg;
-	public $archive_display_enabled;
-	public $sticky_background_video_enabled;
-	public $post_id;
+    public $video_id;
+    public $playlist_id;
+    public $start_time;
+    public $below_header;
+    public $blur_vid_bg;
+    public $archive_display_enabled;
+    public $sticky_background_video_enabled;
+    public $post_id;
 
-	public function __construct() {
+    public function __construct()
+    {
 
-		global $post;
-		if ( null !== $post ) {
-			$this->post_id = $post->ID;
-			$this->get_user_options();
-			$this->determine_display_locations();
-		}
-	}
+        global $post;
+        if (null !== $post) {
+            $this->post_id = $post->ID;
+            $this->get_user_options();
+            $this->determine_display_locations();
+        }
+    }
 
 
-	public function determine_display_locations() {
+    public function determine_display_locations()
+    {
 
-		if ( class_exists( 'WooCommerce' ) ) {
+        if (class_exists('WooCommerce')) {
 
-			if ( ( $this->archive_display_enabled ) && ( is_product_category() ) ) {
-				$this->display();
-			} else {
-				if ( ! is_product_category() ) {
-					$this->display();
-				}
-			}
-		} else {
-			$this->display();
-		}
-	}
+            if (($this->archive_display_enabled) && (is_product_category())) {
+                $this->display();
+            } else {
+                if ( ! is_product_category()) {
+                    $this->display();
+                }
+            }
+        } else {
+            $this->display();
+        }
+    }
 
-	public function display() {
+    public function display()
+    {
 
-		if ( ( '' !== $this->video_id ) || ( '' !== $this->playlist_id ) ) {
+        if (('' !== $this->video_id) || ('' !== $this->playlist_id)) {
 
-			echo "<span id='landing_yt_player' data-id='" . esc_attr( $this->video_id ) . "' data-playlist-id='" . esc_attr( $this->playlist_id ) . "' data-start-time='" . esc_attr( $this->start_time ) . "' data-below-header='" . esc_attr( $this->below_header ) . "' data-blur='" . esc_attr( $this->blur_vid_bg ) . "'></span>";
+            echo "<span id='landing_yt_player' data-id='".esc_attr($this->video_id)."' data-playlist-id='".esc_attr($this->playlist_id)."' data-start-time='".esc_attr($this->start_time)."' data-below-header='".esc_attr($this->below_header)."' data-blur='".esc_attr($this->blur_vid_bg)."'></span>";
 
-		}
+        }
 
-		do_action( 'virtuoso_featured_background_image', $this->post_id );
+        do_action('virtuoso_featured_background_image', $this->post_id);
 
-	}
+    }
 
-	public function get_user_options() {
+    public function get_user_options()
+    {
 
-		$prefix = CustomizerHelpers::get_settings_prefix();
+        $prefix = CustomizerHelpers::get_settings_prefix();
 
-		$video_link     = get_field( 'post_youtube_video_link', $this->post_id, false );
-		$video_id       = Helper::extract_video_id( $video_link );
-		$this->video_id = ( $video_id ) ? $video_id : '';
+        if (function_exists('get_field')) {
+            $video_link     = get_field('post_youtube_video_link', $this->post_id, false);
+            $video_id       = Helper::extract_video_id($video_link);
+            $this->video_id = ($video_id) ? $video_id : '';
 
-		$playlist_link = get_field( 'post_youtube_playlist_link', $this->post_id, false );
+            $playlist_link = get_field('post_youtube_playlist_link', $this->post_id, false);
 
-		if ( ( '' !== $playlist_link ) && ( null !== $playlist_link ) ) {
-			$playlist_id       = explode( '?list=', $playlist_link );
-			$playlist_id       = $playlist_id[1];
-			$this->playlist_id = $playlist_id;
-		} else {
-			$this->playlist_id = '';
-		}
+            if (('' !== $playlist_link) && (null !== $playlist_link)) {
+                $playlist_id       = explode('?list=', $playlist_link);
+                $playlist_id       = $playlist_id[1];
+                $this->playlist_id = $playlist_id;
+            } else {
+                $this->playlist_id = '';
+            }
 
-		$this->start_time                      = get_field( 'start_time', $this->post_id, false );
-		$this->below_header                    = get_field( 'display_below_header', $this->post_id, false );
-		$this->blur_vid_bg                     = get_field( 'blur', $this->post_id, false );
-		$this->sticky_background_video_enabled = get_field( 'fixed_to_page', $this->post_id, false );
+            $this->start_time                      = get_field('start_time', $this->post_id, false);
+            $this->below_header                    = get_field('display_below_header', $this->post_id, false);
+            $this->blur_vid_bg                     = get_field('blur', $this->post_id, false);
+            $this->sticky_background_video_enabled = get_field('fixed_to_page', $this->post_id, false);
 
-	}
+        }
+
+    }
 
 }
--- Lib/Views/header-web-application-menu.php
+++ Lib/Views/header-web-application-menu.php
@@ -1,11 +1,19 @@
 <?php
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
-if ( is_user_logged_in() ) : ?>
-<ul class="virtuoso-user-profile-menu" role="navigation" data-open="false">
-	<li><a href="<?php echo esc_url( bp_loggedin_user_link() ); ?>">Profile</a></li>
-	<li class="user-profile-sep-after"><a href="<?php echo esc_url( bp_loggedin_user_link() ) . 'notifications'; ?>">Notifications</a></li>
-	<li class="user-profile-sep-before"><a href="<?php echo esc_url( bp_loggedin_user_link() ) . 'settings'; ?>">Settings</a></li>
-	<li><a href="<?php echo esc_url( wp_logout_url() ); ?>&redirect_to=/>">Logout</a></li>
-</ul>
+if (is_user_logged_in()) : ?>
+    <?php
+    if (function_exists('bp_loggedin_user_link()')) {
+        $user_link = bp_loggedin_user_link();
+    } else {
+        $user_link = '';
+    }
+    ?>
+	<ul class="virtuoso-user-profile-menu" role="navigation" data-open="false">
+		<li><a href="<?php echo esc_url($user_link); ?>">Profile</a></li>
+		<li class="user-profile-sep-after"><a
+					href="<?php echo esc_url($user_link).'notifications'; ?>">Notifications</a></li>
+		<li class="user-profile-sep-before"><a href="<?php echo esc_url($user_link).'settings'; ?>">Settings</a></li>
+		<li><a href="<?php echo esc_url(wp_logout_url()); ?>&redirect_to=/>">Logout</a></li>
+	</ul>
 <?php endif; ?>
