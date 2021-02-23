2be10eceb0ff111ba646e85cffbfc60dcd038308
Sumner Erhard <develop@sumnererhard.com>
2021-02-23 16:31:57 -0700

    Fixed search auto focus for web application menu

--- assets/js/menu/web-application-menu.js
+++ assets/js/menu/web-application-menu.js
@@ -20,6 +20,7 @@ $(document).ready(function () {
   }
 
   function toggleSearch() {
+
     $('.header-search-container').toggleClass('activated');
 
     $('.header-search-toggle').toggleClass('activated')
@@ -27,7 +28,57 @@ $(document).ready(function () {
     $('.header-search-container').attr('aria-expanded', function (i, attr) {
       return attr === 'true' ? 'false' : 'true'
     });
+
+    let searchInput = $('.header-search-container input[type="text"]');
+
+    if ( searchInput.is(':visible') ) {
+      searchInput
+          .putCursorAtEnd()
+          .on('focus', function() {
+            searchInput.putCursorAtEnd()
+          });
+    }
   }
+
+  $.fn.putCursorAtEnd = function() {
+
+    return this.each(function() {
+
+      // Cache references
+      let $el = $(this),
+          el = this;
+
+      // Only focus if input isn't already
+      if (!$el.is(":focus")) {
+        $el.focus();
+      }
+
+      // If this function exists... (IE 9+)
+      if (el.setSelectionRange) {
+
+        // Double the length because Opera is inconsistent about whether a carriage return is one character or two.
+        let len = $el.val().length * 2;
+
+        // Timeout seems to be required for Blink
+        setTimeout(function() {
+          el.setSelectionRange(len, len);
+        }, 1);
+
+      } else {
+
+        // As a fallback, replace the contents with itself
+        // Doesn't work in Chrome, but Chrome supports setSelectionRange
+        $el.val($el.val());
+
+      }
+
+      // Scroll to the bottom, in case we're in a tall textarea
+      // (Necessary for Firefox and Chrome)
+      this.scrollTop = 999999;
+
+    });
+
+  };
 
   $(document).mouseup(function (e) {
     let secondMenu = $('.header-web-application .virtuoso-user-profile-menu');
